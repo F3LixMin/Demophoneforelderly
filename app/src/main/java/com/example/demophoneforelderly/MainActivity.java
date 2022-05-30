d2324ea9cdb374dccb9f495919e874af2092ffd4
F3LixMin <21008740@myrp.edu.sg>
2022-05-30 22:20:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demophoneforelderly/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.demophoneforelderly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button peter;
+    Button Mary;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        peter.findViewById(R.id.button);
+        Mary.findViewById(R.id.button2);
+
+        peter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 92240336));
+                startActivity(intentCall);
+            }
+        });
+        Mary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 89675543));
+                startActivity(intentCall);
+            }
+        });
+    }
+
+}
